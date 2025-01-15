3287bdabbda1c618306463c41e87c9bda5ae219f
Maddy221 <144130712+Maddy221@users.noreply.github.com>
2025-01-15 09:35:21 -0500

    1/15/25 Added snow and made health

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -3,7 +3,11 @@ extends Node
 
 var coins = 0
 var score = 0
+var cat_position = 0
+var health = 100
 
 func _process(delta: float) -> void:
 	$"GUI/CoinValue".text = str(coins)
 	$"GUI/ScoreValue".text = str(score)
+	$"GUI/Health".text = str(health)
+	#print(progress)
